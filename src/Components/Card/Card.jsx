e5ee40cee73955de972cec00182bd2239f03d7dc
Saifulkhandaker <khsaiful67@gmail.com>
2023-09-14 22:42:13 +0600

    gap and font size decrease in price and credit

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -14,15 +14,15 @@ const Card = ({ card }) => {
             className="w-11/12"
           />
         </figure>
-        <div className="card-body items-center text-center">
+        <div className="card-body items-left text-left">
           <h2 className="card-title text-lg font-bold">{title}</h2>
           <p className='text-lg'>{description}</p>
-          <div className='flex gap-20'>
-            <p className='font-medium text-xl'>$ Price : {price}</p>
+          <div className='flex gap-10'>
+            <p className='font-medium text-lg'>$ Price : {price}</p>
            
            <div className='flex gap-2 mb-3'>
            <p className='mt-2 text-xl text-gray-400'><IoBookSharp></IoBookSharp></p>
-            <p className='font-medium text-xl'>Credit : {credit}hr</p>
+            <p className='font-medium text-lg'>Credit : {credit}hr</p>
            </div>
             
           </div>

bbc29dfea5eb5a0372112e0ef19bd59e71d440c6
Saifulkhandaker <khsaiful67@gmail.com>
2023-09-14 21:58:23 +0600

    cart component added

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Card from "../Card/Card";
+import Cart from "../Cart/Cart";
 const Cards = () => {
     const [cards, setCards] = useState([]);
 
@@ -13,7 +14,7 @@ const Cards = () => {
 
 
     return (
-        <div>
+        <div className='w-11/12 mx-auto md:flex justify-between gap-5'>
             
             <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
             {
@@ -22,6 +23,9 @@ const Cards = () => {
                 card={card}
                 ></Card>))
             }
+            </div>
+            <div className="mt-5">
+                <Cart></Cart>
             </div>
 
             
---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,10 @@
+const Cart = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">Cart</h1>
+            
+        </div>
+    );
+};
+
+export default Cart;
